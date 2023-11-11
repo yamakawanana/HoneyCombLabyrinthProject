b72317f7e8582c32886191006a83df9dddb01f9c
yamakawanana <41903918+yamakawanana@users.noreply.github.com>
2023-11-11 16:47:32 +0900

    Add files via upload

---
+++ HoneyCombLabyrinth/Game/Controller/EnemyController.cpp
@@ -0,0 +1,88 @@
+﻿#include "Game/Controller/EnemyController.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+/// <param name = "event_interface">キャラクターコントローラのイベントインターフェースの参照</param>
+/// <param name = "character">キャラクターの参照</param>
+/// <returns>なし</returns>
+EnemyController::EnemyController(Character& character)
+  : character_(character) {
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+/// <param>なし</param>
+/// <returns>なし</returns>
+EnemyController::~EnemyController() {
+}
+
+/// <summary>
+/// 右上に移動する
+/// </summary>
+/// <param>なし</param>
+/// <returns>上に移動したらtrue、移動出来なかったらfalse</returns>
+bool EnemyController::WalkRightUp() {
+  //event_interface_.OnPushDirectionKey(character_, Field::kRightUp);
+  return true;
+}
+
+/// <summary>
+/// 右下に移動する
+/// </summary>
+/// <param>なし</param>
+/// <returns>下に移動したらtrue、移動出来なかったらfalse</returns>
+bool EnemyController::WalkRightDown() {
+  //event_interface_.OnPushDirectionKey(character_, Field::kRightDown);
+  return true;
+}
+
+/// <summary>
+/// 左上に移動する
+/// </summary>
+/// <param>なし</param>
+/// <returns>上に移動したらtrue、移動出来なかったらfalse</returns>
+bool EnemyController::WalkLeftUp() {
+  //event_interface_.OnPushDirectionKey(character_, Field::kLeftUp);
+  return true;
+}
+
+/// <summary>
+/// 左下に移動する
+/// </summary>
+/// <param>なし</param>
+/// <returns>下に移動したらtrue、移動出来なかったらfalse</returns>
+bool EnemyController::WalkLeftDown() {
+  //event_interface_.OnPushDirectionKey(character_, Field::kLeftDown);
+  return true;
+}
+
+/// <summary>
+/// 右に移動する
+/// </summary>
+/// <param>なし</param>
+/// <returns>右に移動したらtrue、移動出来なかったらfalse</returns>
+bool EnemyController::WalkRight() {
+  //event_interface_.OnPushDirectionKey(character_, Field::kRight);
+  return true;
+}
+
+/// <summary>
+/// 左に移動する
+/// </summary>
+/// <param>なし</param>
+/// <returns>左に移動したらtrue、移動出来なかったらfalse</returns>
+bool EnemyController::WalkLeft() {
+  //event_interface_.OnPushDirectionKey(character_, Field::kLeft);
+  return true;
+}
+
+/// <summary>
+/// ハチがアヒルかクマにぶつかった時の処理
+/// </summary>
+/// <param>なし</param>
+/// <returns>なし</returns>
+void EnemyController::HitCharacter() {
+  //event_interface_.BumpCharacter(character_);
+}
---
+++ HoneyCombLabyrinth/Game/Controller/EnemyController.h
@@ -0,0 +1,84 @@
+﻿#pragma once
+#include "System/Interface/CharacterControllerEventInterface.h"
+#include "System/Character.h"
+
+class EnemyController {
+
+public:
+
+  /// <summary>
+  /// コンストラクタ
+  /// </summary>
+  /// <param name = "event_interface">キャラクターコントローラのイベントインターフェースの参照</param>
+  /// <param name = "character">キャラクターの参照</param>
+  /// <returns>なし</returns>
+  EnemyController(Character& character);
+
+  /// <summary>
+  /// デストラクタ
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>なし</returns>
+  virtual ~EnemyController();
+
+  /// <summary>
+  /// 右上に移動する
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>上に移動したらtrue、移動出来なかったらfalse</returns>
+  bool WalkRightUp();
+
+  /// <summary>
+  /// 右下に移動する
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>下に移動したらtrue、移動出来なかったらfalse</returns>
+  bool WalkRightDown();
+
+  /// <summary>
+  /// 左上に移動する
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>上に移動したらtrue、移動出来なかったらfalse</returns>
+  bool WalkLeftUp();
+
+  /// <summary>
+  /// 左下に移動する
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>下に移動したらtrue、移動出来なかったらfalse</returns>
+  bool WalkLeftDown();
+
+  /// <summary>
+  /// 右に移動する
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>右に移動したらtrue、移動出来なかったらfalse</returns>
+  bool WalkRight();
+
+  /// <summary>
+  /// 左に移動する
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>左に移動したらtrue、移動出来なかったらfalse</returns>
+  bool WalkLeft();
+
+  /// <summary>
+  /// ハチがアヒルかクマにぶつかった時の処理
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>なし</returns>
+  void HitCharacter();
+
+private:
+
+  /// <summary>
+  /// キャラクターコントローラのイベントインターフェース
+  /// </summary>
+  //CharacterControllerEventInterface& event_interface_;
+
+  /// <summary>
+  /// キャラクター
+  /// </summary>
+  Character& character_;
+};
