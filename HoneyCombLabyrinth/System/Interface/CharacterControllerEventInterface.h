5a7f82071363820ca56f722852677ceb837604b0
yamakawanana <41903918+yamakawanana@users.noreply.github.com>
2023-11-11 17:26:08 +0900

    Add files via upload

---
+++ HoneyCombLabyrinth/System/Interface/CharacterControllerEventInterface.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "System/LogConsole.h"
+#include "System/Character.h"
+#include "Game/Field.h"
+
+/// <summary>
+/// CharacterControllerEventInterfaceクラス
+/// </summary>
+class CharacterControllerEventInterface {
+public:
+
+  /// <summary>
+  /// デストラクタ
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>なし</returns>
+  virtual ~CharacterControllerEventInterface() {
+    DEBUG_PRINT(CharacterControllerEventInterfaceのデストラクタ);
+  }
+
+  /// <summary>
+  /// 各方向キーが押された時のイベント
+  /// </summary>
+  /// <param name>なし</param>
+  /// <returns>なし</returns>
+  virtual void OnPushDirectionKey(Character& character, Field::DirectionType direction) = 0;
+
+  /// <summary>
+  /// キャラクターがぶつかるイベント
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>なし</returns>
+  virtual void BumpCharacter(Character& character) = 0;
+
+};
---
+++ HoneyCombLabyrinth/System/Interface/MenuControllerEventInterface.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "System/LogConsole.h"
+
+/// <summary>
+/// MenuControllerEventInterfaceクラス
+/// </summary>
+class MenuControllerEventInterface  {
+public:
+
+  /// <summary>
+  /// デストラクタ
+  /// </summary>
+  /// <param>なし</param>
+  /// <returns>なし</returns>
+  virtual ~MenuControllerEventInterface() {
+    DEBUG_PRINT(MenuControllerEventInterfaceのデストラクタ);
+  }
+
+  /// <summary>
+  /// 決定ボタンの押下が押された時のイベント
+  /// </summary>
+  /// <param name>なし</param>
+  /// <returns>なし</returns>
+  virtual void OnPushDecide() = 0;
+
+  /// <summary>
+  /// 終了ボタンが押された時のイベント
+  /// </summary>
+  /// <param name>なし</param>
+  /// <returns>なし</returns>
+  virtual void OnPushFinish() = 0;
+
+  /// <summary>
+  /// ポーズボタンが押された時のイベント
+  /// </summary>
+  /// <param name>なし</param>
+  /// <returns>なし</returns>
+  virtual void OnPushPause() = 0;
+
+  /// <summary>
+  /// BackSpaceボタンが押された時のイベント
+  /// </summary>
+  /// <param name>なし</param>
+  /// <returns>なし</returns>
+  virtual void OnPushBackSpace() = 0;
+};
